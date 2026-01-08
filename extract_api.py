3e9141a71bac9d4b539e0dfa3bfcee377e74836d
harveyjoyce <harveyjoyce56@outlook.com>
2026-01-08 11:44:17 +0000

    set up first zip file extraction

--- extract_api.py
+++ extract_api.py
@@ -1,3 +1,66 @@
+import os
 import requests
+import logging
+import json
+from dotenv import load_dotenv
+from datetime import datetime
 
-print('hi')
+logs_dir = 'logs'
+if os.path.exists(logs_dir):
+    pass
+else:
+    os.mkdir(logs_dir)
+filename = datetime.now().strftime('%Y-%m-%d %H-%M-%S')
+log_filename = f"logs/{filename}.log"
+
+dir = 'data'
+if os.path.exists(dir):
+    pass
+else:
+    os.mkdir(dir)
+filepath = f'{dir}/{filename}.zip'
+
+# Configure logs to retrieve INFO messages and higher
+logging.basicConfig(
+    level=logging.INFO, 
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename=log_filename
+)
+
+logger = logging.getLogger()
+
+
+load_dotenv()
+
+AMPLITUDE_API_KEY = os.getenv("AMP_API_KEY")
+AMPLITUDE_SECRET_KEY = os.getenv("AMP_SECRET_KEY")
+logger.info("AMP Environment Variables Read")
+
+# error if can't find api keys
+
+url = 'https://analytics.eu.amplitude.com/api/2/export'
+
+params= {
+        'start': '20260101T00', 
+        'end': '20260101T23'
+        }
+logger.info("API URL/Parameters Set")
+
+response = requests.get(url, params=params, auth=(AMPLITUDE_API_KEY, AMPLITUDE_SECRET_KEY))
+logger.info(f"API Call Response Code: '{response.status_code}'")
+
+if response.status_code == 200:
+    data = response.content 
+    logger.info("Data retrieved successfully.")
+    logger.info("Saving data to data.zip")
+    with open(filepath, 'wb') as file:
+        file.write(data)
+    logger.info("Data saved to data.zip")
+else:
+    logger.error(f"API Call Error '{response.status_code}: {response.text}'")
+    print(f'Error {response.status_code}: {response.text}')
+
+logger.info("Process Finished")
+
+
+
